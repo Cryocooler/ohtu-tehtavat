d6c9b2b5eeb49b55dd838d4ca7b8e7a566dd2a71
Sahin Dursun <sahin.dursun@relexsolutions.com>
2022-03-13 13:18:41 +0200

    TDD-ostoskori step 4: kahden eri tuotteen lisaamisen jalkeen ostoskorissa kaksi tavaraa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,7 +5,8 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-
+        self._maito = Tuote("Maito", 3)
+        self._apa = Tuote("APA", 5)
     #step 1
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
 
@@ -18,4 +19,22 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    #step3
+    def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self._apa)
+        self.kori.lisaa_tuote(self._maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    #step5
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_tuotteiden_hintojen_summa(self):
+        self.kori.lisaa_tuote(self._maito)
+        self.kori.lisaa_tuote(self._apa)
 
